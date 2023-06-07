cefd5c5d16f5c3cf8863b41a6888aafd33e9c5cb
Yigitcan Olmez <Yigitcan.Olmez@d-teknoloji.com.tr>
2023-06-07 08:04:14 +0300

    rabbitmq connected

--- OrderAPI/Program.cs
+++ OrderAPI/Program.cs
@@ -5,16 +5,19 @@ using OrderAPI.Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddMassTransit(x=>
+builder.Services.AddMassTransit(x =>
 {
-    //x.UsingRabbitMq
+    x.UsingRabbitMq((context, conf) =>
+    {
+        conf.Host(builder.Configuration.GetConnectionString("RabbitMQ"));
+    });
 });
- builder.Services.AddDbContext<AppDbContext>(opt =>
- {
-     opt.UseSqlServer(builder.Configuration.GetConnectionString("Sql"));
- });
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("Sql"));
+});
 
-
+builder.Services.AddMassTransitHostedService();     
 // Add services to the container
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
