55c4eeba87fdbc641bdb91957489cad17b8c3693
Yiğitcan Ölmez <yigitcan.olmez@d-teknoloji.com.tr>
2024-01-20 18:11:03 +0300

    added OrderStateMachine

--- SagaStateMachineWorkerService/Program.cs
+++ SagaStateMachineWorkerService/Program.cs
@@ -1,12 +1,47 @@
+using MassTransit;
+using MessageBroker;
+using Microsoft.EntityFrameworkCore;
 using SagaStateMachineWorkerService;
+using SagaStateMachineWorkerService.Models;
+using System.Reflection;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        var configuration = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+           .Build();
+
         var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddMassTransit(conf =>
+        {
+            conf.AddSagaStateMachine<OrderStateMachine, OrderStateInstance>().EntityFrameworkRepository(opt =>
+            {//state machine'nýn sql server kullanýlacaðý belirtildi.
+                opt.AddDbContext<DbContext, OrderStateDbContext>((provider, builder) =>
+                {
+                    builder.UseSqlServer(configuration.GetConnectionString("SqlCon"), m =>
+                    {
+                        m.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+                    });
+                });
+            });
+            conf.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(configure =>
+            {
+                configure.Host(configuration.GetConnectionString("RabbitMQ"));
+
+                configure.ReceiveEndpoint(RabbitMQSettingsConst.OrderSaga, e =>
+                {
+                    e.ConfigureSaga<OrderStateInstance>(provider);
+                });
+            }));
+        });
+
+
+
         services.AddHostedService<Worker>();
     })
     .Build();
