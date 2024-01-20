0d4441155ed5d815d71d02f7851437a3650afa46
Yiğitcan Ölmez <yigitcan.olmez@d-teknoloji.com.tr>
2024-01-20 18:35:56 +0300

    added State For Order State Machine

--- SagaStateMachineWorkerService/Models/OrderStateMachine.cs
+++ SagaStateMachineWorkerService/Models/OrderStateMachine.cs
@@ -1,13 +1,49 @@
 ﻿using Automatonymous;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MessageBroker.Interfaces;
 
 namespace SagaStateMachineWorkerService.Models
 {
     public class OrderStateMachine : MassTransitStateMachine<OrderStateInstance>
     {
+        public Event<IOrderCreatedRequestEvent> OrderCreatedRequestEvent { get; set; }
+        public State OrderCreated { get; private set; }
+
+        public OrderStateMachine()
+        {
+            InstanceState(x => x.CurrentState);
+
+            Event(() => OrderCreatedRequestEvent, y => y.CorrelateBy<int>(x => x.OrderId, z => z.Message.OrderId).SelectId(context => Guid.NewGuid()));//eventten gelene order ID ile karşılaştır.
+
+            Initially(When(OrderCreatedRequestEvent).Then(context =>
+            {
+                // Instance => db
+                // Data=> eventten gelen veriler
+                context.Instance.BuyerId = context.Data.BuyerId;
+                context.Instance.OrderId = context.Data.OrderId;
+                context.Instance.Created = DateTime.Now;
+                context.Instance.CardName = context.Data.Payment.CardName;
+                context.Instance.CardNumber = context.Data.Payment.CardNumber;
+                context.Instance.CVV = context.Data.Payment.CVV;
+                context.Instance.Expiration = context.Data.Payment.Expiration;
+                context.Instance.TotalPrice = context.Data.Payment.TotalPrice;
+            }).Then(context =>
+            {
+                Console.WriteLine($"OrderCreatedRequestEvent before : {context.Instance.ToString()}");
+            }).TransitionTo(OrderCreated).Then(context =>
+            {
+                Console.WriteLine($"OrderCreatedRequestEvent after : {context.Instance.ToString()}");
+            }));
+
+
+
+
+
+
+        }
+
+
+
+
+
     }
 }
